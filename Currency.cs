004b961fc690f6db75630aa04c1c42d20022e8f0
nihatkhidirov <nihatmkh@code.edu.az>
2024-03-18 15:41:52 +0400

    First Commit

---
+++ Currency.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyEnum_Indexer
+{
+
+    internal enum Currency
+    {
+        Usd=1,
+        Euro,
+        Try
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿namespace CurrencyEnum_Indexer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Currency currency = Currency.Try;
+            double value = 970;
+            double converting = Exchange(currency, value);
+           
+        }
+        public static double Exchange(Currency currency, double value) //usd 
+        {
+            double azntousdt = 0.59;
+            double azntoeur = 0.54;
+            double azntotry = 19;
+            double converted = 0;
+            if(Enum.IsDefined(typeof(Currency), currency))
+            {
+                switch (currency)
+                {
+                    case Currency.Usd:
+                        converted = value * azntousdt;
+                        Console.WriteLine($"Mubadileye esasen: {value} AZN = {converted} USD ");
+
+                        break;
+                    case Currency.Euro:
+                        converted = value * azntoeur;
+                        Console.WriteLine($"Mubadileye esasen: {value} AZN = {converted} EUR ");
+
+                        break;
+                    case Currency.Try:
+                        converted = value * azntotry;
+                        Console.WriteLine($"Mubadileye esasen: {value} AZN = {converted} TRY ");
+
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Duzgun valyuta daxil edilmemisdir!");
+            }
+            return converted;
+        }
+    }
+}
